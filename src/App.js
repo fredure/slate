52879f2b6d25d66d6fd672e8a267ce696943bd3a
nv.ugrinovskiy <nv.ugrinovskiy@tensor.ru>
2020-12-03 22:28:44 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useState } from 'react'
+import { createEditor, Editor } from 'slate'
 
-function App() {
+import { Slate, Editable, withReact, useSlate } from 'slate-react'
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.code) {
+    children = <code>{children}</code>
+  }
+
+  return <span {...attributes}>{children}</span>
+}
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format)
+
+  if (isActive) {
+    Editor.removeMark(editor, format)
+  } else {
+    Editor.addMark(editor, format, true)
+  }
+}
+
+const MarkButton = ({ format }) => {
+  const editor = useSlate()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`button ${isMarkActive(editor, format) ? 'active' : ''}`}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleMark(editor, format)
+      }}
+    >
+      code
     </div>
-  );
+  )
+}
+
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor)
+  return marks ? marks[format] === true : false
+}
+
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [
+        { text: '' },
+      ],
+    },
+  ]);
+  
+  const onChangeCallback = (state) => {
+    setValue(state);
+  }
+
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={onChangeCallback}
+    >
+      <div className="tooblar">
+        <MarkButton format="code" />
+      </div>
+      <Editable 
+        className="editor"
+        spellCheck
+        renderLeaf={renderLeaf}
+      />
+    </Slate>
+  )
 }
 
 export default App;
