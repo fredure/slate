98b09ef9cd94eb55e5a3fd86075ffe11844e2358
nv.ugrinovskiy <nv.ugrinovskiy@tensor.ru>
2020-12-04 10:46:14 +0300

    change to component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React from 'react'
 import { createEditor, Editor } from 'slate'
 
-import { Slate, Editable, withReact, useSlate } from 'slate-react'
+import { Slate, Editable, withReact } from 'slate-react'
 
 const Leaf = ({ attributes, children, leaf }) => {
   if (leaf.code) {
@@ -11,69 +11,85 @@ const Leaf = ({ attributes, children, leaf }) => {
   return <span {...attributes}>{children}</span>
 }
 
-const toggleMark = (editor, format) => {
-  const isActive = isMarkActive(editor, format)
+class MarkButton extends React.Component {
+  constructor(props) {
+    super(props);
 
-  if (isActive) {
-    Editor.removeMark(editor, format)
-  } else {
-    Editor.addMark(editor, format, true)
+    this.editor = props.editor;
+    this.format = props.format;
   }
-}
 
-const MarkButton = ({ format }) => {
-  const editor = useSlate()
-  return (
-    <div
-      className={`button ${isMarkActive(editor, format) ? 'active' : ''}`}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleMark(editor, format)
-      }}
-    >
-      code
-    </div>
-  )
-}
-
-const isMarkActive = (editor, format) => {
-  const marks = Editor.marks(editor)
-  return marks ? marks[format] === true : false
-}
-
-const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
-
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [
-        { text: '' },
-      ],
-    },
-  ]);
+  toggleMark = () => {
+    const isActive = this.isMarkActive()
   
-  const onChangeCallback = (state) => {
-    setValue(state);
+    if (isActive) {
+      Editor.removeMark(this.editor, this.format)
+    } else {
+      Editor.addMark(this.editor, this.format, true)
+    }
   }
 
-  return (
-    <Slate
-      editor={editor}
-      value={value}
-      onChange={onChangeCallback}
-    >
-      <div className="tooblar">
-        <MarkButton format="code" />
+  isMarkActive = () => {
+    const marks = Editor.marks(this.editor)
+    return marks ? marks[this.format] === true : false
+  }
+
+  render() {
+    return (
+      <div
+        className={`button ${this.isMarkActive() ? 'active' : ''}`}
+        onMouseDown={event => {
+          event.preventDefault()
+          this.toggleMark()
+        }}
+      >
+        code
       </div>
-      <Editable 
-        className="editor"
-        spellCheck
-        renderLeaf={renderLeaf}
-      />
-    </Slate>
-  )
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: [
+        {
+          type: 'paragraph',
+          children: [
+            { text: '' },
+          ],
+        },
+      ]
+    }
+
+    this.onChangeCallback = value => this.setState({value});
+
+    this.editor = withReact(createEditor());
+    this.renderLeaf = props => <Leaf {...props} />;
+  }
+
+  render() {
+    return (
+      <Slate
+        editor={this.editor}
+        value={this.state.value}
+        onChange={this.onChangeCallback}
+      >
+        <div className="tooblar">
+          <MarkButton 
+            format="code"
+            editor={this.editor} />
+        </div>
+        <Editable 
+          className="editor"
+          spellCheck
+          renderLeaf={this.renderLeaf}
+        />
+      </Slate>
+    )
+  }
 }
 
 export default App;
